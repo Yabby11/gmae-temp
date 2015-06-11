f5a8fea476ca915c05a3bb1a0d381b041a8f49b4
Jake Andrews <jakeandrews.karatekid@gmail.com>
2015-06-11 14:59:07 +1000

    backup

---
+++ 2.5D Shooter/Assets/ai.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ai : MonoBehaviour {
+	playerController player;
+
+	public float visionDistence = 20f;
+	public Transform visionPoint;
+	public float vision; 
+	public float visionAngle = 30f;
+	public float moveSpeed = 2f;
+	public float ChaseDistence = 3f;
+
+	// Use this for initialization
+
+
+	void Start () {
+	
+
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+		}
+
+	void Look() {
+		Vector3 deltaToPlayer = player.transform.position - visionPoint.position;
+		Vector3 directionToPlayer = deltaToPlayer.normalized;
+		float distenceToPlayer = directionToPlayer.magnitude;
+		
+			float dot = Vector3.Dot (visionPoint.forward, directionToPlayer);
+
+			if (dot < 0)
+			{
+			return;
+			}
+
+			if (distenceToPlayer > visionDistence)
+			{
+			return;
+			}
+
+			float angle = Vector3.Angle (transform.forward, directionToPlayer);
+
+			if (angle > visionAngle)
+			{
+				return;
+			}
+
+			RaycastHit hit;
+
+			if (Physics.Raycast (transform.position, directionToPlayer, out hit, visionDistence))
+			{
+				if(hit.collider.gameObject == player.gameObject)
+				{
+					return;
+				}
+	
+				
+
+		}
+
+	}
+}
---
+++ 2.5D Shooter/Assets/playerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(CharacterController))]
+
+public class playerController : MonoBehaviour {
+	public float turnSpeed = 300;
+	public float inputTurn;
+	public float movementForward; 
+	public float movementSide;
+	public float movementSpeed = 5;
+
+	CharacterController charecterController; 
+
+	void Awake () {
+
+				charecterController = GetComponent<CharacterController> ();
+		}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		inputTurn = Input.GetAxis ("Mouse X");
+
+		movementForward = Input.GetAxis ("Horizontal");
+		movementSide = Input.GetAxis ("Vertical");
+			
+	}
+	void FixedUpdate()
+	{
+		transform.Rotate (Vector3.up, inputTurn * turnSpeed * Time.fixedDeltaTime);
+
+			Vector3 delta = Vector3.zero; 
+
+			delta += transform.forward * movementForward * movementSpeed * Time.fixedDeltaTime;
+			delta += transform.right * movementSide * movementSpeed * Time.fixedDeltaTime;
+
+			charecterController.Move (delta);
+
+			//charecterController.Move (
+		}
+}
